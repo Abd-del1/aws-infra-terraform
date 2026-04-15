fdb9f53802d0e333000d936248b9bbc2f7fccf36
abdz <a6dullahali123@gmail.com>
2026-04-15 15:57:58 +0530

    Refactor EC2 security group and add SSH key management

--- main.tf
+++ main.tf
@@ -10,19 +10,20 @@ data "aws_subnets" "default" {
 }
 
 resource "aws_security_group" "ec2_sg" {
-  name        = "simple-ec2-sg"
-  description = "Allow SSH"
+  name        = "${var.instance_name}-sg"
+  description = "Allow SSH only from approved CIDR"
   vpc_id      = data.aws_vpc.default.id
 
   ingress {
-    description = "SSH"
+    description = "SSH from approved CIDR"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = [var.allowed_ssh_cidr]
   }
 
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
@@ -30,16 +31,41 @@ resource "aws_security_group" "ec2_sg" {
   }
 
   tags = {
-    Name = "simple-ec2-sg"
+    Name = "${var.instance_name}-sg"
   }
+}
+
+resource "tls_private_key" "ec2_ssh" {
+  algorithm = "RSA"
+  rsa_bits  = 4096
+}
+
+resource "aws_key_pair" "generated" {
+  key_name   = "${var.instance_name}-key"
+  public_key = tls_private_key.ec2_ssh.public_key_openssh
+
+  tags = {
+    Name = "${var.instance_name}-key"
+  }
+}
+
+resource "vault_kv_secret_v2" "ec2_ssh_key" {
+  mount = "secret"
+  name  = "ec2/${var.instance_name}/ssh"
+
+  data_json = jsonencode({
+    key_name    = aws_key_pair.generated.key_name
+    private_key = tls_private_key.ec2_ssh.private_key_pem
+    public_key  = tls_private_key.ec2_ssh.public_key_openssh
+  })
 }
 
 resource "aws_instance" "simple_ec2" {
   ami                         = var.ami_id
   instance_type               = var.instance_type
-  key_name                    = var.key_name
   subnet_id                   = data.aws_subnets.default.ids[0]
   vpc_security_group_ids      = [aws_security_group.ec2_sg.id]
+  key_name                    = aws_key_pair.generated.key_name
   associate_public_ip_address = true
 
   tags = {
