a08e44937224fcbb4fd981f47605b65cb932da7c
abdz <a6dullahali123@gmail.com>
2026-04-15 15:57:24 +0530

    Refine variable descriptions and add Vault variables
    Updated variable descriptions for clarity and added new variables for Vault integration.

--- variables.tf
+++ variables.tf
@@ -5,7 +5,7 @@ variable "aws_region" {
 }
 
 variable "instance_name" {
-  description = "EC2 instance name"
+  description = "Name of the EC2 instance"
   type        = string
   default     = "simple-ec2"
 }
@@ -17,12 +17,24 @@ variable "instance_type" {
 }
 
 variable "ami_id" {
-  description = "AMI ID for EC2"
+  description = "AMI ID for the EC2 instance"
   type        = string
   default     = "ami-0c02fb55956c7d316"
 }
 
-variable "key_name" {
-  description = "Existing AWS EC2 key pair name"
+variable "allowed_ssh_cidr" {
+  description = "CIDR block allowed to SSH into the EC2 instance"
   type        = string
+}
+
+variable "vault_addr" {
+  description = "Vault server address"
+  type        = string
+  default     = "http://184.73.22.166:8200"
+}
+
+variable "vault_token" {
+  description = "Vault token used by Terraform to write generated SSH key into Vault"
+  type        = string
+  sensitive   = true
 }
