da1f2388675430f296140f8f40c5b73822b7d9fd
abdz <a6dullahali123@gmail.com>
2026-04-15 15:57:00 +0530

    Add TLS and Vault providers to Terraform configuration

--- providers.tf
+++ providers.tf
@@ -6,9 +6,22 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0"
+    }
+    vault = {
+      source  = "hashicorp/vault"
+      version = "~> 4.0"
+    }
   }
 }
 
 provider "aws" {
   region = var.aws_region
+}
+
+provider "vault" {
+  address = var.vault_addr
+  token   = var.vault_token
 }
