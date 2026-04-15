65c5d16e5be58fc38c48cdeb224386a1148a5b6f
abdz <a6dullahali123@gmail.com>
2026-04-15 15:41:31 +0530

    Add output values for EC2 instance details

--- outputs.tf
+++ outputs.tf
@@ -1,1 +1,14 @@
-# Output values
+output "instance_id" {
+  description = "EC2 instance ID"
+  value       = aws_instance.simple_ec2.id
+}
+
+output "public_ip" {
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.simple_ec2.public_ip
+}
+
+output "private_ip" {
+  description = "Private IP of the EC2 instance"
+  value       = aws_instance.simple_ec2.private_ip
+}
