c36de5de34d5b1c019b7e57416b5285ac6024ef6
abdz <a6dullahali123@gmail.com>
2026-04-15 15:58:30 +0530

    Enhance outputs with descriptions and new entries
    Added descriptions to existing outputs and created new outputs for key name and SSH secret path.

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,24 @@
 output "instance_id" {
-  value = aws_instance.simple_ec2.id
+  description = "ID of the created EC2 instance"
+  value       = aws_instance.simple_ec2.id
 }
 
 output "public_ip" {
-  value = aws_instance.simple_ec2.public_ip
+  description = "Public IP of the EC2 instance"
+  value       = aws_instance.simple_ec2.public_ip
 }
 
 output "private_ip" {
-  value = aws_instance.simple_ec2.private_ip
+  description = "Private IP of the EC2 instance"
+  value       = aws_instance.simple_ec2.private_ip
 }
 
-output "default_vpc_id" {
-  value = data.aws_vpc.default.id
+output "generated_key_name" {
+  description = "Generated AWS key pair name"
+  value       = aws_key_pair.generated.key_name
+}
+
+output "vault_ssh_secret_path" {
+  description = "Vault path where generated SSH keys are stored"
+  value       = "secret/ec2/${var.instance_name}/ssh"
 }
